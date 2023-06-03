5dde4fa746747e76b893261c743a0358ab5d7a83
Maxim Derbenev <maksym.derbenov@hit.zone>
2023-06-02 22:50:05 -0400

    new nodes

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 module "gke_cluster" {
-  source         = "git::https://github.com/dereban25/prom-terra-modules.git"
+  source         = "git::https://github.com/dereban25/prom-terra-modules.git//modules/eks"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
